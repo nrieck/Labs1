2c456566d88c73f67d2a9d877ef863cffb23f06c
nrieck <nrieck@10.18.12.4>
2017-08-31 15:15:38 -0500

    Added cons.

---
+++ src/lab1/Employee.java
@@ -0,0 +1,69 @@
+package lab1;
+
+import java.util.Date;
+
+/**
+ * Fix the code in this class to do PROPERTY encapsulation correctly. Also
+ * consider if any of the properties should be mandatory and use a 
+ * constructor to enforce that. Review the tips in the document 
+ * "EncapCheckList.pdf" if needed.
+ *
+ * @author      Jim Lombardo, WCTC Instructor
+ * @version     1.02
+ */
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private boolean metWithHr;
+    private boolean metDeptStaff;
+    private boolean reviewedDeptPolicies;
+    private boolean movedIn;
+    private String cubeId;
+    private Date orientationDate;
+
+    public Employee(String firstName, String lastName, String ssn, boolean reviewedDeptPolicies, String cubeId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ssn = ssn;
+        this.reviewedDeptPolicies = reviewedDeptPolicies;
+        this.cubeId = cubeId;
+    }
+    
+    
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    
+
+}
