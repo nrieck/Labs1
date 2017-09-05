44d7a7913a94a00d07f30d2ecfcb2d9a159b155a
Nick <Nick@192.168.0.77>
2017-09-05 02:08:49 -0500

    Lab 4 Adding classes and a few methods

---
+++ src/lab4/Company.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+
+/**
+ *
+ * @author Nick
+ */
+public class Company {
+    
+    private HrWork hr;
+    
+    public Company() {
+        hr = new HrWork();
+    }
+    
+    public void hireEmployee(String firstName, String lastName, String ssn) {
+        hr.hireEmployee(firstName, lastName, ssn);
+        
+    }
+
+    public HrWork getHr() {
+        return hr;
+    }
+
+    public void setHr(HrWork hr) {
+        this.hr = hr;
+    }
+}
---
+++ src/lab4/HrWork.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Nick
+ */
+public class HrWork {
+    
+    private List<lab4.solution.Employee> employees;
+
+
+    public HrWork() {
+        employees = new ArrayList();
+    }
+    
+    public void hireEmployee(String firstName, String lastName, String ssn) {
+        
+    }
+}
